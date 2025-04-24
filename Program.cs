8eb9aba980d6e7aaf32dd7fe00663eb6ba1b5d1f
Soomin Song <song.soomin.418@outlook.com>
2025-04-24 10:16:23 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/// 2. Sum digits in string
+/// Given a string, write a method which returns
+/// sum of all digits in that string. Assume that
+/// string contains only single digits.
+/// Expected input and output
+/// SumDigitsInString("1q2w3e") → 6
+/// SumDigitsInString("L0r3m.1p5um") → 9
+/// SumDigitsInString("") → 0
+
+Console.Write("Input phrase: ");
+Console.WriteLine(SumDigitsInString(Console.ReadLine()));
+
+int SumDigitsInString(string input)
+{
+    int sum = 0;
+    foreach (char c in input)
+    {
+        if(char.IsDigit(c)) sum=sum+(Convert.ToInt32(c)-48);
+    }
+    return sum;
+}
